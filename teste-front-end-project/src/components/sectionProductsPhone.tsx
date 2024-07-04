0ee309f9ff9e89142dcdd28819a79bf79ef23971
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-04 15:40:37 -0300

    feat: create title

--- teste-front-end-project/src/components/sectionProductsPhone.tsx
+++ teste-front-end-project/src/components/sectionProductsPhone.tsx
@@ -2,5 +2,11 @@ import '../assets/style/sectionProductsPhone.scss';
 
 export default function SectionProductsPhone(){
     return (
+        <section>
+            <div className='container'>
+                <h1>Produtos relacionados</h1>
+                <div className='line'></div>
+            </div>
+        </section>
     )
 }
