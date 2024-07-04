3bc78eef6f658bea8ede6f54a522ab7494330b3a
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-04 15:41:57 -0300

    feat: add SectionProductsPhone

--- teste-front-end-project/src/components/main.tsx
+++ teste-front-end-project/src/components/main.tsx
@@ -1,11 +1,13 @@
 import SectionBanner from "./sectionBanner";
-import SectionCards from "./sectionCards"
+import SectionCards from "./sectionCards";
+import SectionProductsPhone from './sectionProductsPhone';
 
 export default function Main(){
     return (
         <main>
             <SectionBanner/>
             <SectionCards/>
+            <SectionProductsPhone/>
         </main>
     )
 }
