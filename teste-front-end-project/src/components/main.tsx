856db6365a71d545a1a9f618d476fe6b844f6c17
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-04 14:56:07 -0300

    feat: add Card section component

--- teste-front-end-project/src/components/main.tsx
+++ teste-front-end-project/src/components/main.tsx
@@ -1,8 +1,11 @@
 import SectionBanner from "./sectionBanner";
+import SectionCards from "./sectionCards"
+
 export default function Main(){
     return (
         <main>
             <SectionBanner/>
+            <SectionCards/>
         </main>
     )
 }
