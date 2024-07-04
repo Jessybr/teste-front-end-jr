73eae420db4d35cbb9b0d72c06705627907171df
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-04 15:41:06 -0300

    feat: create section products

--- teste-front-end-project/src/components/sectionProductsPhone.tsx
+++ teste-front-end-project/src/components/sectionProductsPhone.tsx
@@ -1,11 +1,22 @@
 import '../assets/style/sectionProductsPhone.scss';
 
 export default function SectionProductsPhone(){
+    const arrayProducts = ["Celular","Acessórios","Tablets","Notebooks","Tvs","Ver todos"];
+    const arrayStyle = ["pink"];
+
     return (
         <section>
             <div className='container'>
                 <h1>Produtos relacionados</h1>
                 <div className='line'></div>
+            </div>
+            <div className='container-products'>
+                {arrayProducts.map((text, ind) => (
+                    <div className='products'>
+                        <span className={arrayStyle[ind]}>{text}</span>
+                    </div>
+                ))}
+                
             </div>
         </section>
     )
