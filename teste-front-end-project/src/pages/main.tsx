d5e5dcde18a409c35defb2a39dc8e44b87d0a1d9
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-05 16:44:30 -0300

    chore: move files to new directory

--- teste-front-end-project/src/pages/main.tsx
+++ teste-front-end-project/src/pages/main.tsx
@@ -1,7 +1,7 @@
 import SectionBanner from "../components/sectionBanner/sectionBanner";
 import SectionProducts from '../components/products/sectionProducts';
-import OtherProducts from "../otherProducts/otherProducts";
-import SectionPartner from "../sectionPartner/sectionPartner";
+import OtherProducts from "../components/otherProducts/otherProducts";
+import SectionPartner from "../components/sectionPartner/sectionPartner";
 
 export default function Main(){
     return (
