ea1d64d3810762c142c660138cfe33aa5611911a
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-05 20:41:22 -0300

    chore: delete img tag

--- teste-front-end-project/src/components/sectionBanner/sectionBanner.tsx
+++ teste-front-end-project/src/components/sectionBanner/sectionBanner.tsx
@@ -5,11 +5,12 @@ export default function SectionBanner(){
     return (
         < >
             <section className='section-banner'>
-                <h1>Venha conhecer nossas promoções</h1>
-                <h2>50% Off nos produtos</h2>
-                <button>Ver produto</button>
+                <div className="info">
+                    <h1>Venha conhecer nossas promoções</h1>
+                    <h2>50% Off nos produtos</h2>
+                    <button>Ver produto</button>
+                </div>
             </section>
-            <img className='banner' src={Banner} alt="" />
         </>
     )
 }
