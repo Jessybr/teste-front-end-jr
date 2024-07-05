4afe94b26d39a6f82e8a530f40fafa6729a99676
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-05 20:44:47 -0300

    chore: edit h1 and h4 text

--- teste-front-end-project/src/components/titleSession/titleSession.tsx
+++ teste-front-end-project/src/components/titleSession/titleSession.tsx
@@ -7,8 +7,9 @@ interface TitleProps {
 export default function TitleSession({text} : TitleProps){
     return(
         <div className='container'>
-            <h1>{text}</h1>
+            <h1>Produtos relacionados</h1>
             <div className='line'></div>
+            <h4>{text}</h4>
         </div>
     )
 }
