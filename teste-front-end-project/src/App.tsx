451ac4aaa13bb9c46e2254ece9d4b88798d0805b
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-05 16:39:13 -0300

    chore: rename directories

--- teste-front-end-project/src/App.tsx
+++ teste-front-end-project/src/App.tsx
@@ -1,5 +1,5 @@
-import Header from './components/header'
-import Main from './components/main'
+import Header from './components/header/header'
+import Main from './pages/main'
 
 function App() {
 
