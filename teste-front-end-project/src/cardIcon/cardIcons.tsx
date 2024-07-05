00e340411ee3ea2a589912ed1bfa04f07f5d5cdf
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-05 16:32:45 -0300

    feat: create CardIcon component

---
+++ teste-front-end-project/src/cardIcon/cardIcons.tsx
@@ -0,0 +1,18 @@
+import './cardIcons.scss';
+
+interface CardProps {
+    imageSrc: string;
+    text: string;
+    style: string;
+}
+
+export default function CardIcons({imageSrc, text, style} : CardProps){
+    return(
+        <div className="card-icons">
+            <div className={`container-icon ${style}`}>
+                <img src={imageSrc} alt="" />
+            </div>
+            <span>{text}</span>
+        </div>
+    )
+}
