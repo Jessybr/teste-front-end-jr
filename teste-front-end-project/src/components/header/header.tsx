5d691c1d22276521db2d2451ce793b40c750cb90
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-06 17:53:44 -0300

    chore: rename path and classNames

--- teste-front-end-project/src/components/header/header.tsx
+++ teste-front-end-project/src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import './header.scss';
+import style from './header.module.scss';
 import ShieldCheck from '../../assets/images/ShieldCheck.png';
 import Truck from '../../assets/images/Truck.png'
 import CreditCard from '../../assets/images/CreditCard.png';
@@ -13,42 +13,42 @@ import CrownSimple from '../../assets/images/CrownSimple.png';
 export default function Header(){
     return(
         <header>
-            <div className="info">
-                <div className="info-text">
+            <div className={style.info}>
+                <div className={style.infoText}>
                     <img src={ShieldCheck} alt="" />
                     <p>Compra <span>100% segura</span></p>
                 </div>
-                <div className="info-text">
+                <div className={style.infoText}>
                     <img src={Truck} alt="" />
                     <p><span>Frete grátis</span> acima de R$ 200</p>
                 </div>
-                <div className="info-text">
+                <div className={style.infoText}>
                     <img src={CreditCard} alt="" />
                     <p><span>Parcele</span> suas compras</p>
                 </div>
             </div>
 
             <nav>
-                <div className='nav-search'>
+                <div className={style.navSearch}>
                     <img src={Vtex} alt="" />
-                    <div className="search">
+                    <div className={style.search}>
                         <span>O que você está buscando?</span>
                         <img src={MagnifyingGlass} alt="Magnifying Glass image" />
                     </div>
-                    <div className="icons">
+                    <div className={style.styleIcons}>
                         <img src={Packege} alt="" />
                         <img src={Heart} alt="" />
                         <img src={UserCircle} alt="" />
                         <img src={ShoppingCart} alt="" />
                     </div>
                 </div>
-                <div className='nav-sessions'>
+                <div className={style.navSessions}>
                     <span>Todas categorias</span>
                     <span>Supermercado</span>
                     <span>Livros</span>
                     <span>Moda</span>
                     <span>Lançamentos</span>
-                    <span className='pink'>Ofertas do dia</span>
+                    <span className={style.pink}>Ofertas do dia</span>
                     <div>
                         <img src={CrownSimple} alt="" />
                         <span>Assinatura</span>
