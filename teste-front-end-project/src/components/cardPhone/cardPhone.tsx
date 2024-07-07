beb1dd36e79eb35f735c258328115b5378457b03
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-06 21:57:25 -0300

    feat: add action

--- teste-front-end-project/src/components/cardPhone/cardPhone.tsx
+++ teste-front-end-project/src/components/cardPhone/cardPhone.tsx
@@ -5,23 +5,28 @@ interface ProductProps {
     productName: string;
     price: number;
     photo: string;
-    
+    btnAction: (ind: number) => void;
 }
 
-export default function CardPhone({ind, productName, price, photo} : ProductProps){
+export default function CardPhone({ind, productName, price, photo, btnAction} : ProductProps){
+    const priceOld = price + 400;
+
     return (
-        <div className={style.cardPhone}>
-            <div className={style.info} key={ind}>
-                <div className={style.cardHeader}>
-                    <img src={photo} alt={productName} />
-                    <h3>{productName}</h3>
+        <>
+            <div className={style.cardPhone}>
+                <div className={style.info}>
+                    <div className={style.cardHeader}>
+                        <img src={photo} alt={productName} />
+                        <h3>{productName}</h3>
+                    </div>
+                    <span className={style.priceOld}>R${priceOld.toLocaleString('pt-BR', {minimumFractionDigits: 2})}</span>
+                    <span className={style.price}>R${price.toLocaleString('pt-BR', {minimumFractionDigits: 2})}</span>
+                    <span className={style.rate}>ou 2x de R$ 49,95 sem juros</span>
+                    <span className={style.freight}>Frete grátis</span>
+                    <button onClick={()=>btnAction(ind)}>Comprar</button>
                 </div>
-                <span className={style.priceOld}>R${(price.toFixed(2)) + 200}</span>
-                <span className={style.price}>R${price.toLocaleString('pt-BR', {minimumFractionDigits: 2})}</span>
-                <span className={style.rate}>ou 2x de R$ 49,95 sem juros</span>
-                <span className={style.freight}>Frete grátis</span>
-                <button>Comprar</button>
             </div>
-        </div>
+            
+        </>
     )
 }
